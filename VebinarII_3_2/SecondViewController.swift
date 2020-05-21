3c8824589e4f2038ab7fb7dae700665ad3696364
Alexey Burer <ecspert.07@gmail.com>
2020-05-21 09:22:58 +0300

    Setup UI

---
+++ VebinarII_3_2/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  VebinarII_3_2
+//
+//  Created by Алексей Кукушкин on 21.05.2020.
+//  Copyright © 2020 Алексей Кукушкин. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

4e59a781d46deb890e15a4a2a1c7eee53c2ee588
Alexey Burer <ecspert.07@gmail.com>
2020-05-21 08:56:35 +0300

    Initial Commit

---
+++ VebinarII_3_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VebinarII_3_2
+//
+//  Created by Алексей Кукушкин on 21.05.2020.
+//  Copyright © 2020 Алексей Кукушкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
